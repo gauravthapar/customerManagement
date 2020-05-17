80ff540b5c17889f9b7d96d11494c2506d2787a1
gauravthapar <gauravthapar34@gmail.com>
2020-05-17 17:11:54 +0530

    password change and reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name="home"),
@@ -14,4 +15,30 @@ urlpatterns = [
     path('signup/',views.signupuser, name = 'signupuser'),
     path('login/',views.loginuser, name ="loginuser"),
     path('logout/',views.logoutuser, name = 'logoutuser'),
+
+    path('password_reset/',
+        auth_views.PasswordResetView.as_view(template_name = 'accounts/password_reset.html'), 
+        name = "password_reset"
+    ),
+    path('password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(template_name = 'accounts/password_reset_sent.html'), 
+        name = "password_reset_done"
+    ),
+    path('password_reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(template_name = 'accounts/password_reset_form.html'), 
+        name = "password_reset_confirm"
+    ),
+    path('password_reset/complete/',
+        auth_views.PasswordResetCompleteView.as_view(template_name = 'accounts/password_reset_done.html'), 
+        name = "password_reset_complete"
+    ),
+    path('password_change',
+        auth_views.PasswordChangeView.as_view(template_name = 'accounts/password_change.html'), 
+        name = "password_change"
+    ),
+
+    path('password_change/done',
+        auth_views.PasswordChangeDoneView.as_view(template_name = 'accounts/password_change_done.html'), 
+        name = "password_change_done"
+    ),
 ]
--- customerManagement/settings.py
+++ customerManagement/settings.py
@@ -129,3 +129,11 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'gauravt.crio.ta@gmail.com'
+EMAIL_HOST_PASSWORD = 'thapar1998'
